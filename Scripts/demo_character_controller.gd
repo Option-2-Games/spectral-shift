d0adbe8a3edfb552263c811e2a2b25392ffd1e5a
Microbob µ <microbob@is88.com>
2022-07-27 00:03:15 -0700

    Super basic character movement

--- Scripts/demo_character_controller.gd
+++ Scripts/demo_character_controller.gd
@@ -1,16 +1,21 @@
-extends Node
+extends Sprite
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+const speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		scale *= 1.1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
