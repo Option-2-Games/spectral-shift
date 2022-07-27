96762301c1cd00d1c70c6f5613ebe26fe5fe73da
Microbob µ <microbob@is88.com>
2022-07-27 00:27:37 -0700

    ignore venv, linted and formatted

--- Scripts/demo_character_controller.gd
+++ Scripts/demo_character_controller.gd
@@ -1,21 +1,27 @@
 extends Sprite
 
-const speed = 200
+# Player movement SPEED
+const SPEED = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
+
+# Detect one-off input events.
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		scale *= 1.1
 
+
+# Called every frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
+		position.x -= SPEED * delta
 	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
+		position.x += SPEED * delta
 	if Input.is_action_pressed("ui_up"):
-		position.y -= speed * delta
+		position.y -= SPEED * delta
 	if Input.is_action_pressed("ui_down"):
-		position.y += speed * delta
+		position.y += SPEED * delta
