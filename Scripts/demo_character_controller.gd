00cada13fa1208f3718295a6a86e2fec1f31660a
Microbob µ <microbob@is88.com>
2022-07-30 10:58:38 -0700

    Added dummy documentation

--- Scripts/demo_character_controller.gd
+++ Scripts/demo_character_controller.gd
@@ -1,21 +1,28 @@
 extends Sprite
 
-# Player movement SPEED
+# Constant: Player movement speed
 const SPEED = 200
 
 
+# Function: _ready
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass  # Replace with function body.
 
 
+# Function: _input
 # Detect one-off input events.
+# Parameters:
+#  event - triggered event
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		scale *= 1.1
 
 
+# Function: _process
 # Called every frame.
+# Parameters:
+#	delta - Different in time between frames
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		position.x -= SPEED * delta
