cdf119aee26529b2029c64e5a22d4d9bd05189c2
Microbob µ <microbob@is88.com>
2022-09-02 01:05:45 -0700

    First pass demo of spectrum shifting

--- scripts/spectrum_lamp.gd
+++ scripts/spectrum_lamp.gd
@@ -6,6 +6,7 @@ extends Area2D
 
 # Const: BORDER_ROTATION_SPEED
 # How fast the lamp borders rotate
+#
 # Multiply by delta to get the rotation increment amount
 const BORDER_ROTATION_SPEED: int = 2
 
@@ -13,15 +14,27 @@ const BORDER_ROTATION_SPEED: int = 2
 # How far out from the center the lamp borders will rotate
 const BORDER_ROTATION_PATH_RADIUS: int = 7
 
+# Const: CLOSE_RED_SPECTRUM_ANIMATION_KEY
+# Animation key to close the red spectrum
+const CLOSE_RED_SPECTRUM_ANIMATION_KEY: String = "close_red_spectrum"
+
+# Const: CLOSE_GREEN_SPECTRUM_ANIMATION_KEY
+# Animation key to close the green spectrum
+const CLOSE_GREEN_SPECTRUM_ANIMATION_KEY: String = "close_green_spectrum"
+
+# Const: CLOSE_BLUE_SPECTRUM_ANIMATION_KEY
+# Animation key to close the blue spectrum
+const CLOSE_BLUE_SPECTRUM_ANIMATION_KEY: String = "close_blue_spectrum"
+
 # === Component Paths ===
 
 export var red_lamp_path: NodePath
 export var green_lamp_path: NodePath
 export var blue_lamp_path: NodePath
 
-export var red_border_path: NodePath
-export var green_border_path: NodePath
-export var blue_border_path: NodePath
+export var red_animation_player_path: NodePath
+export var green_animation_player_path: NodePath
+export var blue_animation_player_path: NodePath
 
 # === Variables ===
 
@@ -31,37 +44,39 @@ var _rotation_progress: float
 
 # Var: _lamp_state
 # Current state on/off state of the spectrums
+#
 # Use a binary number to represent the state of each lamp
 #
-# 1st bit: base reality, not considered in usage
-# 2nd bit: red spectrum
-# 3rd bit: green spectrum
-# 4th bit: blue spectrum
+# - 1st bit: base reality, not considered in usage
+# - 2nd bit: red spectrum
+# - 3rd bit: green spectrum
+# - 4th bit: blue spectrum
+#
+# Default: 1 (only base reality is on)
 #
 # Example:
 # 	- 0b1111 = 15 = all lamps are on (this situation will never happen through gameplay)
 # 	- 0b0100 = 4 = only green lamp is on
-var _lamp_state: int = 14
+var _lamp_state: int = 1
+
+# Var: _lamp_display_state
+# Current visual state of the lamps
+#
+# May be different from _lamp_state if the state was just changed and the
+# animation has not been applied yet
+var _lamp_display_state: int = 1
 
 # === Components Nodes ===
 
 onready var _red_lamp: Light2D = get_node(red_lamp_path)
 onready var _green_lamp: Light2D = get_node(green_lamp_path)
 onready var _blue_lamp: Light2D = get_node(blue_lamp_path)
-onready var _lamps: Array = [_red_lamp, _green_lamp, _blue_lamp]
 
-onready var _red_border: Sprite = get_node(red_border_path)
-onready var _green_border: Sprite = get_node(green_border_path)
-onready var _blue_border: Sprite = get_node(blue_border_path)
-onready var _borders: Array = [_red_border, _green_border, _blue_border]
+onready var _red_animation_player: AnimationPlayer = get_node(red_animation_player_path)
+onready var _green_animation_player: AnimationPlayer = get_node(green_animation_player_path)
+onready var _blue_animation_player: AnimationPlayer = get_node(blue_animation_player_path)
 
 # === Built-in Functions ===
-
-
-# Func: _ready
-# Setup the lamp by disabling all spectrums
-func _ready():
-	reset_to_base()
 
 
 # Func: _process
@@ -112,7 +127,7 @@ func _process(delta: float):
 func _unhandled_input(event: InputEvent):
 	if event is InputEventKey:
 		if Input.is_action_pressed("exclude_all_spectrums"):
-			reset_to_base()
+			use_spectrum(Constants.Spectrum.BASE)
 		elif Input.is_action_pressed("exclude_red_spectrum"):
 			exclude_spectrum(Constants.Spectrum.RED)
 		elif Input.is_action_pressed("exclude_green_spectrum"):
@@ -126,79 +141,54 @@ func _unhandled_input(event: InputEvent):
 
 # Func: use_spectrum
 # Set lamp to a specific spectrum.
+#
 # Use <Constants.Spectrum> to define the spectrums to use.
 # Does <reset_to_base> if a value outside of the 3 spectrums (that are not BASE) is passed.
 #
 # Parameters:
 #   spectrum - <Constants.Spectrum> to use
 func use_spectrum(spectrum: int):
-	if spectrum > 0 and spectrum <= 3:
-		var desired_state = 1 << spectrum
-
-		if !(desired_state >> Constants.Spectrum.RED & 1):
-			_disable_spectrum_visualization(Constants.Spectrum.RED)
-		if !(desired_state >> Constants.Spectrum.GREEN & 1):
-			_disable_spectrum_visualization(Constants.Spectrum.GREEN)
-		if !(desired_state >> Constants.Spectrum.BLUE & 1):
-			_disable_spectrum_visualization(Constants.Spectrum.BLUE)
-
-		_enable_spectrum_visualization(spectrum)
-	else:
-		reset_to_base()
+	_lamp_state = 1 << spectrum + 1 if spectrum > 0 and spectrum <= 3 else 1
+	_run_open_close_animation()
 
 
 # Func: exclude_spectrum
 # Set lamp to enable base + two spectrums and exclude one.
+#
 # Use <Constants.Spectrum> to define the spectrums to exclude.
-# Does <reset_to_base> if a value outside of the 3 spectrums (that are not BASE) is passed.
+# Closes all spectrums if a value outside of the 3 spectrums is passed.
 #
 # Parameters:
 #   spectrum - <Constants.Spectrum> to exclude
 func exclude_spectrum(spectrum: int):
-	if spectrum > 0 and spectrum <= 3:
-		var desired_state = 14 - (1 << spectrum)
-
-		if desired_state >> Constants.Spectrum.RED & 1:
-			_enable_spectrum_visualization(Constants.Spectrum.RED)
-		if desired_state >> Constants.Spectrum.GREEN & 1:
-			_enable_spectrum_visualization(Constants.Spectrum.GREEN)
-		if desired_state >> Constants.Spectrum.BLUE & 1:
-			_enable_spectrum_visualization(Constants.Spectrum.BLUE)
-
-		_disable_spectrum_visualization(spectrum)
-	else:
-		reset_to_base()
-
-
-# Func: reset_to_base
-# Turn off spectrums in lamp (only show base).
-func reset_to_base():
-	_disable_spectrum_visualization(Constants.Spectrum.RED)
-	_disable_spectrum_visualization(Constants.Spectrum.GREEN)
-	_disable_spectrum_visualization(Constants.Spectrum.BLUE)
-
-	_lamp_state = 1
+	_lamp_state = 15 - (1 << spectrum) if spectrum > 0 and spectrum <= 3 else 1
+	_run_open_close_animation()
 
 
 # === Helper Functions ===
 
 
-# Func: _disable_spectrum_visualization
-# Turn off visualization of a spectrum.
-func _disable_spectrum_visualization(spectrum: int):
-	if _is_spectrum_on(spectrum):
-		_lamps[spectrum - 1].energy = 0
-		_borders[spectrum - 1].visible = false
-		_lamp_state &= ~(1 << spectrum)
-
-
-# Func: _enable_spectrum_visualization
-# Turn on visualization of a spectrum.
-func _enable_spectrum_visualization(spectrum: int):
-	if !_is_spectrum_on(spectrum):
-		_lamps[spectrum - 1].energy = 1
-		_borders[spectrum - 1].visible = true
-		_lamp_state |= 1 << spectrum
+# Func: _run_open_close_animation
+# Run the open/close animation for each lamp based on <_lamp_state> and <_lamp_display_state>
+func _run_open_close_animation():
+	for spectrum in range(1, 4):
+		var should_be_on = _is_spectrum_on(spectrum)
+		if should_be_on != _is_spectrum_visually_on(spectrum):
+			var direction = -1 if should_be_on else 1
+			match spectrum:
+				Constants.Spectrum.RED:
+					_red_animation_player.play(
+						CLOSE_RED_SPECTRUM_ANIMATION_KEY, -1, direction, should_be_on
+					)
+				Constants.Spectrum.GREEN:
+					_green_animation_player.play(
+						CLOSE_GREEN_SPECTRUM_ANIMATION_KEY, -1, direction, should_be_on
+					)
+				Constants.Spectrum.BLUE:
+					_blue_animation_player.play(
+						CLOSE_BLUE_SPECTRUM_ANIMATION_KEY, -1, direction, should_be_on
+					)
+	_lamp_display_state = _lamp_state
 
 
 # Func: _is_spectrum_on
@@ -208,6 +198,15 @@ func _enable_spectrum_visualization(spectrum: int):
 #   spectrum - <Constants.Spectrum> to check
 func _is_spectrum_on(spectrum: int) -> bool:
 	return (_lamp_state & 1 << spectrum) >> spectrum == 1
+
+
+# Func: _is_spectrum_visually_on
+# Same as <_is_spectrum_on> but for the display state
+#
+# Parameters:
+#   spectrum - <Constants.Spectrum> to check
+func _is_spectrum_visually_on(spectrum: int) -> bool:
+	return (_lamp_display_state & 1 << spectrum) >> spectrum == 1
 
 
 # Func: _handle_item_entrance
