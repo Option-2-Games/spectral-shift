c77b6a1ed2ba54a52d9333d1ff1ca86d17e07403
Microbob µ <kjy5@uw.edu>
2022-09-12 23:24:42 -0700

    Convert to SceneTreeTween

--- scripts/spectrum_switcher.gd
+++ scripts/spectrum_switcher.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 export(NodePath) var cover_path
 export(NodePath) var state_label_path
-export(NodePath) var tween_runner_path
+
+var _tweener: PropertyTweener
 
 var open_state: bool
 var selecting_color: int
@@ -10,47 +11,44 @@ var selected_color: int
 
 onready var _cover: Sprite = get_node(cover_path)
 onready var _state_label: Label = get_node(state_label_path)
-onready var _tween_runner: Tween = get_node(tween_runner_path)
 
 func _ready():
+	# Hide by default
 	modulate.a = 0
 
 
 func _unhandled_input(event):
-	var tween_state: bool
 	if event is InputEventMouseMotion:
-		if open_state:
-			var mouse_pos = event.position - global_position
+		var mouse_pos = event.position - global_position
+		if open_state and mouse_pos.length_squared() >= 16900:
 			mouse_pos.y *= -1
 			var mouse_angle_adjusted = mouse_pos.angle_to(Vector2(-1, -1))
 			var mouse_angle_degrees = rad2deg(mouse_angle_adjusted)
 
-			var current_cover_rotation = _cover.rotation
 			_state_label.set_text("ON")
 			if mouse_angle_degrees >= 0 and mouse_angle_degrees < 90:
-				tween_state = _tween_runner.interpolate_property(_cover, "rotation", current_cover_rotation, deg2rad(180), 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(180), 0.25)
 				selecting_color = Constants.Spectrum.RED
 			elif mouse_angle_degrees >= 90 and mouse_angle_degrees < 180:
-				tween_state = _tween_runner.interpolate_property(_cover, "rotation", current_cover_rotation, deg2rad(-90), 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(-90), 0.25)
 				selecting_color = Constants.Spectrum.GREEN
 			elif mouse_angle_degrees >= -90 and mouse_angle_degrees < 0:
-				tween_state = _tween_runner.interpolate_property(_cover, "rotation", current_cover_rotation, deg2rad(90), 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(90), 0.25)
 				selecting_color = Constants.Spectrum.BASE
 				_state_label.set_text("OFF")
 			elif mouse_angle_degrees >= -180 and mouse_angle_degrees < -90:
-				tween_state = _tween_runner.interpolate_property(_cover, "rotation", current_cover_rotation, 0, 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(0), 0.25)
 				selecting_color = Constants.Spectrum.BLUE
-			if tween_state:
-				tween_state = _tween_runner.start()
 	elif event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			open_state = event.is_pressed()
 			if open_state:
 				global_position = event.position
-				tween_state = _tween_runner.interpolate_property(_tween_runner.get_parent(), "modulate:a", 0, 1, 0.1, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+				_tweener = _create_tween().tween_property(_cover.get_parent(), "modulate:a", 1.0, 0.1)
 			else:
-				tween_state = _tween_runner.interpolate_property(_tween_runner.get_parent(), "modulate:a", 1, 0, 0.2, Tween.TRANS_CUBIC, Tween.EASE_IN)
+				_tweener = create_tween().tween_property(_cover.get_parent(), "modulate:a", 0.0, 0.25)
 				selected_color = selecting_color
-			
-			if tween_state:
-				tween_state = _tween_runner.start()
+
+
+func _create_tween() -> SceneTreeTween:
+	return get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
