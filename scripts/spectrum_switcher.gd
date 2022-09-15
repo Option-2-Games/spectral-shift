9dadab85084dbf0acbd25c1cbe7fb83be95ee12b
Microbob µ <kjy5@uw.edu>
2022-09-14 20:29:25 -0700

    Animate open and close

--- scripts/spectrum_switcher.gd
+++ scripts/spectrum_switcher.gd
@@ -12,9 +12,10 @@ var selected_color: int
 onready var _cover: Node2D = get_node(cover_path)
 onready var _state_label: Label = get_node(state_label_path)
 
+
 func _ready():
 	# Hide by default
-	modulate.a = 0
+	scale = Vector2.ZERO
 
 
 func _unhandled_input(event):
@@ -31,23 +32,39 @@ func _unhandled_input(event):
 			var cover_current_rotation = deg2rad(_cover.rotation_degrees)
 			if mouse_angle_degrees >= 0 and mouse_angle_degrees < 90:
 				# Selecting RED = 180º
-				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(180), 0.25)
+				_tweener = _create_tween_for_rotation().tween_property(
+					_cover, "rotation", deg2rad(180), 0.25
+				)
 				selecting_color = Constants.Spectrum.RED
 			elif mouse_angle_degrees >= 90 and mouse_angle_degrees < 180:
 				# Selecting GREEN = 270º
 
-				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(270), 0.25).from(cover_current_rotation if selecting_color != Constants.Spectrum.BLUE else cover_current_rotation + TAU)
-				
+				_tweener = _create_tween_for_rotation().tween_property(_cover, "rotation", deg2rad(270), 0.25).from(
+					(
+						cover_current_rotation
+						if selecting_color != Constants.Spectrum.BLUE
+						else cover_current_rotation + TAU
+					)
+				)
+
 				selecting_color = Constants.Spectrum.GREEN
 			elif mouse_angle_degrees >= -180 and mouse_angle_degrees < -90:
 				# Selecting BLUE = 0º
 
-				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(0), 0.25).from(cover_current_rotation if selecting_color != Constants.Spectrum.GREEN else cover_current_rotation - TAU)
+				_tweener = _create_tween_for_rotation().tween_property(_cover, "rotation", deg2rad(0), 0.25).from(
+					(
+						cover_current_rotation
+						if selecting_color != Constants.Spectrum.GREEN
+						else cover_current_rotation - TAU
+					)
+				)
 
 				selecting_color = Constants.Spectrum.BLUE
 			elif mouse_angle_degrees >= -90 and mouse_angle_degrees < 0:
 				# Selecting BASE = 90º
-				_tweener = _create_tween().tween_property(_cover, "rotation", deg2rad(90), 0.25)
+				_tweener = _create_tween_for_rotation().tween_property(
+					_cover, "rotation", deg2rad(90), 0.25
+				)
 				selecting_color = Constants.Spectrum.BASE
 				_state_label.set_text("OFF")
 	elif event is InputEventMouseButton:
@@ -55,11 +72,21 @@ func _unhandled_input(event):
 			open_state = event.is_pressed()
 			if open_state:
 				global_position = event.position
-				_tweener = _create_tween().tween_property(_cover.get_parent(), "modulate:a", 1.0, 0.1)
+				_tweener = _create_tween_for_scale().tween_property(_cover.get_parent(), "scale", Vector2.ONE, 0.1).set_ease(
+					Tween.EASE_OUT
+				)
 			else:
-				_tweener = create_tween().tween_property(_cover.get_parent(), "modulate:a", 0.0, 0.25)
+				_tweener = _create_tween_for_scale().tween_property(_cover.get_parent(), "scale", Vector2.ZERO, 0.25).set_ease(
+					Tween.EASE_IN
+				)
 				selected_color = selecting_color
 
 
-func _create_tween() -> SceneTreeTween:
-	return get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+func _create_tween_for_rotation() -> SceneTreeTween:
+	return get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_CUBIC).set_ease(
+		Tween.EASE_OUT
+	)
+
+
+func _create_tween_for_scale() -> SceneTreeTween:
+	return get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_BACK)
