f2379a45c51abc6d6a66b71fd63371e17a4f5281
Microbob µ <microbob@is88.com>
2023-03-22 16:58:31 -0700

    merge region test scene

---
+++ tests/scenes/spectrum_merge_region/spectrum_merge_region_tester.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# Components
+onready var _merge_region = $SpectrumMergeRegion
+
+# Properties
+var _merge_region_open = true
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		if _merge_region_open:
+			_merge_region.close()
+		else:
+			_merge_region.open()
+
+		_merge_region_open = !_merge_region_open
