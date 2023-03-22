5a62762c2a4aece45b4bff6a7c2e570cd4efade0
Microbob µ <microbob@is88.com>
2023-03-22 15:42:11 -0700

    Return-to-3.5 (#49)
    * Update requirements and project
    
    * Fix some import errors

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -1,10 +1,10 @@
 extends Node
-# Collection of global constants
+## Collection of global constants
 
-# Enum: Spectrum
-#
-# BASE - Base reality, no spectrums merged
-# RED - Red spectrum merged
-# GREEN - Green spectrum merged
-# BLUE - Blue spectrum merged
+## Spectrum enum
+##
+## BASE - Base reality, no spectrums merged
+## RED - Red spectrum merged
+## GREEN - Green spectrum merged
+## BLUE - Blue spectrum merged
 enum Spectrum { BASE, RED, GREEN, BLUE }
