4fcfcda61238d5f0a9a0292d95dc9336e54ccc35
Microbob µ <microbob@is88.com>
2023-03-22 20:00:19 -0700

    Extended Mergable

--- scripts/mergable.gd
+++ scripts/mergable.gd
@@ -1,10 +1,18 @@
 extends CollisionObject2D
 class_name Mergable
 
+var _merges_count: int = 0
+
+
 func entered_merge_region() -> void:
+	_merges_count += 1
 	self.set_collision_layer_bit(0, true)
 	self.set_collision_mask_bit(0, true)
 
+
 func exited_merge_region() -> void:
-	self.set_collision_layer_bit(0, false)
-	self.set_collision_mask_bit(0, false)
+	if _merges_count > 0:
+		_merges_count -= 1
+	if _merges_count == 0:
+		self.set_collision_layer_bit(0, false)
+		self.set_collision_mask_bit(0, false)
